aa2a1def0c155a30b9c82873aade6f11be38bfa7
Praveen <satyammaurya9620@gmail.com>
2024-01-04 00:25:25 +0530

    Doing building screen1

---
+++ Components/Shared/Main/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../../ui/Navbar/page'
+import Screen1 from '../../ui/Screen1/page'
+const page = () => {
+  return (
+    <>
+    <div className="container">
+    <Navbar/>
+     <Screen1/>
+    </div>
+         
+
+    </>
+  )
+}
+
+export default page
---
+++ Components/ui/Navbar/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+const page = () => {
+  return (
+    <>
+    <div className="navbar">
+       <div className="navbar-logo">
+           <h1>INCLUSIFY</h1>
+       </div>
+       <div className="menu">
+          <h1>Menu</h1>
+       </div>
+    </div>
+       
+    </>
+  )
+}
+
+export default page
