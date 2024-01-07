97826b7e52d8895b691d1e85e4061aab1fe44652
Dhanashree Mhatre <93194086+dhanashreemhatre@users.noreply.github.com>
2024-01-07 07:29:14 -0800

    Update views.py

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,11 +2,11 @@ from rest_framework.response import Response
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from .serializers import AccountSerializers
-from rest_framework.parsers import JSONParser
-from django.http import JsonResponse
-from rest_framework.decorators import api_view, parser_classes
+from .models import Account
 from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
 User=settings.AUTH_USER_MODEL
+import datetime,jwt
 # Create your views here.
 
 
@@ -19,5 +19,26 @@ class RegisterView(APIView):
 
 class LoginView(APIView):
     def post(self,request):
-        pass  
+        email=request.data['email']
+        password=request.data['password']
+        try:
+            user=  Account.objects.get(email=email)
+        except:
+            user=None
+        if user is None:
+            raise AuthenticationFailed("User Not Found")
+        if not user.check_password(password):
+            raise AuthenticationFailed("Incorrect password!")
+        
+        payload={
+            'id':user.id,
+            'exp':datetime.datetime.utcnow()+datetime.timedelta(minutes=60),
+            'iat':datetime.datetime.utcnow()
+        }
+        token=jwt.encode(payload,'secret',algorithm='HS256')
+        resopnse=Response()
+        resopnse.set_cookie(key='jwt',value=token,httponly=True)
+        resopnse.data={'jwt':token}
+        
+        return resopnse
    
