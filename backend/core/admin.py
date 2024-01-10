d2cb48e9735c5d89706e3722a62b038b225a1a24
Dhanashree Mhatre <93194086+dhanashreemhatre@users.noreply.github.com>
2024-01-10 10:19:07 +0530

    Delete backend directory

--- backend/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Product,ProductAttribute,ProductAttributeValue,ProductImages,ProductReview
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(ProductAttribute)
-admin.site.register(ProductAttributeValue)
-admin.site.register(ProductImages)
-admin.site.register(ProductReview)
